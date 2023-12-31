a47dc7e762f84f815a8067471611ffb61359fe27
Aleksa Jovanovic <aleksa.j.1996@gmail.com>
2023-12-31 16:51:46 +0100

    Update

--- frontend/src/ContactSupport.js
+++ frontend/src/ContactSupport.js
@@ -1,9 +1,11 @@
-import { Form, Row, Col } from 'react-bootstrap';
+import { Form, Row, Col, Button } from 'react-bootstrap';
 
 function ContactSupport(props) {
 	return (
 		<Form className="mt-5 mb-5">
-			<h2 className="mb-5 text-primary-color text-center">Contact Support</h2>
+			<h2 className="mb-5 text-primary-color text-center">
+				Contact Support
+			</h2>
 			<Row>
 				<Col>
 					<Form.Control className="mb-4" placeholder="Name" />
@@ -16,6 +18,18 @@ function ContactSupport(props) {
 						rows={6}
 						placeholder="Message"
 					/>
+				</Col>
+			</Row>
+			<Row>
+				<Col className="text-center">
+					<Button
+						className="mt-3 btn-background-color px-5"
+						variant="primary"
+						type="button"
+						size="md"
+					>
+						Submit
+					</Button>
 				</Col>
 			</Row>
 		</Form>
